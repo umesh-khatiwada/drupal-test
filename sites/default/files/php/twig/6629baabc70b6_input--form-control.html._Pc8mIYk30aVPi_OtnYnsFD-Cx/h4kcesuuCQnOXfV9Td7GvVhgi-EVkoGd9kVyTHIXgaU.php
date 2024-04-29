986a093431a1229bb7ca4301fd2b6f809b62ded3
DevOps pro <devopsintopros@gmail.com>
2024-04-29 15:58:57 +0000

    theme added

---
+++ sites/default/files/php/twig/6629baabc70b6_input--form-control.html._Pc8mIYk30aVPi_OtnYnsFD-Cx/h4kcesuuCQnOXfV9Td7GvVhgi-EVkoGd9kVyTHIXgaU.php
@@ -0,0 +1,123 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/bootstrap/templates/input/input--form-control.html.twig */
+class __TwigTemplate_4b46ced4a42490d3f16905b4f976c41e extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'input' => [$this, 'block_input'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "input.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("input.html.twig", "themes/bootstrap/templates/input/input--form-control.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["attributes"]);    }
+
+    // line 24
+    public function block_input($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        ob_start(function () { return ''; });
+        // line 25
+        echo "    ";
+        // line 26
+        $context["classes"] = ["form-control"];
+        // line 30
+        echo "    <input";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [($context["classes"] ?? null)], "method", false, false, true, 30), 30, $this->source), "html", null, true);
+        echo " />
+  ";
+        $___internal_parse_2_ = ('' === $tmp = ob_get_clean()) ? '' : new Markup($tmp, $this->env->getCharset());
+        // line 24
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(twig_spaceless($___internal_parse_2_));
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "themes/bootstrap/templates/input/input--form-control.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  64 => 24,  58 => 30,  56 => 26,  54 => 25,  49 => 24,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/bootstrap/templates/input/input--form-control.html.twig", "/home/hamrohr/public_html/themes/bootstrap/templates/input/input--form-control.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("apply" => 24, "set" => 26);
+        static $filters = array("escape" => 30, "spaceless" => 24);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['apply', 'set'],
+                ['escape', 'spaceless'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
